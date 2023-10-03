dbec0ea764eb8b80d92afd053097a7b144e1dc30
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-10-03 18:45:54 -0300

    uri

--- database/src/dataBase/index.js
+++ database/src/dataBase/index.js
@@ -1,6 +1,6 @@
 const { mongoose } = require("mongoose");
 const { MONGO_URI } = require("../config/env");
-const con = mongoose.createConnection("mongodb+srv://starwar:3844010Pep@cluster0.byqlo13.mongodb.net/star_wars");
+const con = mongoose.createConnection(MONGO_URI);
 
 
 module.exports={
